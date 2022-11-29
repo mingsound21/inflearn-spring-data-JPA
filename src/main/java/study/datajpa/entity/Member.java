07a8036bc81c915ca48076456e66b2ff117417ab
mingsound <62479120+mingsound21@users.noreply.github.com>
2022-11-29 15:13:05 +0900

    ch 02. 예제 도메인 모델과 동작확인

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -1,26 +1,49 @@
 package study.datajpa.entity;
 
 import lombok.*;
+import org.springframework.context.annotation.EnableMBeanExport;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter @Setter // setter는 가급적 없는 게 GOOD(실무에서는 꼭 필요할 때만 setter를 쓰거나, 별도의 비즈니스 네이밍된 함수를 사용)
 @NoArgsConstructor(access = AccessLevel.PROTECTED) // @Entity 기본 생성자 필요, 아무곳에서나 사용하지 않도록 PROTECTED
+@ToString(of = {"id", "username", "age"}) // "team" 추가하면 안됨(양방향 연관관계 일 때, ToString 무한 루프 주의)
 public class Member {
     @Id
     @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
     private String username;
+    private int age;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    // 생성자
     public Member(String username) {
         this.username = username;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if(team != null){ // 일단은 team == null이면 무시
+            changeTeam(team);
+        }
     }
 
     // setter 대신 이런 메서드 생성하는 것이 GOOD
     public void changeUsername(String username){
         this.username = username;
     }
+
+    // 연관관계 편의 메서드
+    public void changeTeam(Team team){
+        this.team = team;
+        team.getMembers().add(this);
+    }
+
+
 }
---
+++ src/main/java/study/datajpa/entity/Team.java
@@ -0,0 +1,29 @@
+package study.datajpa.entity;
+
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id", "name"})
+public class Team {
+    @Id
+    @GeneratedValue
+    @Column(name = "team_id")
+    private Long id;
+    private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
+    // 생성자
+    public Team(String name) {
+        this.name = name;
+    }
+}
